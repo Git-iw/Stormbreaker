2dcfeadf40422af1bf887d47d26485ec46a5f904
Git-iw <141666971+Git-iw@users.noreply.github.com>
2024-05-29 13:07:14 +0100

    Initialized the required functions.

--- bitboard.py
+++ bitboard.py
@@ -1,6 +1,25 @@
 class Bitboard:
-    def __init__(self, size):
+    def __init__(self, board=0): # Not requried to use type annotation as I know it
+        self.board = board # It is initialized 0 because I will use bitwise operators, If i can.
+
+    def set_bit(self, postition: int):
+        ...
+    
+    def clear_bit(self, position: int):
+        ...
+    
+    def isBit_set(self, position: int):
+        ...
+    
+    def print_board(self) -> None:
+        """Prints a visual representation of the bitboard (for debugging)."""
         ...
 
-    def set_square(self, row, col):
-        ...
+def main():
+    bitboard = Bitboard(8)
+    bitboard.print_board()
+
+if __name__ == "__main__":
+    main()
+    # Yes, I will stop camel case for most of the part.
+    # I will start to use type annotations just for my understanding sake.
