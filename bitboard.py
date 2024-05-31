c4468e045b275eaa28058255d819e524f661c554
Git-iw <141666971+Git-iw@users.noreply.github.com>
2024-05-31 15:35:17 +0100

    differentiation between white and black pieces.

--- bitboard.py
+++ bitboard.py
@@ -1,46 +1,76 @@
 class Bitboard:
-    def __init__(self, board=0): # Not requried to use type annotation as I know it
-        self.board = board # It is initialized 0 because I will use bitwise operators, If i can.
+    def __init__(self, black_board=0, white_board=0): # Not requried to use type annotation as I know it
+        self.white_board = white_board 
+        self.black_board = black_board
 
-    def set_bit(self, position: int):
+    def set_bit(self, position: int, color: str):
+        color = color.lower()
         if not (0 <= position <= 63):
             raise ValueError("Given bit is not within the board range!")
+        if color == "black":
+            self.black_board |= 1 << position
+        elif color == "white":
+            self.white_board |= 1 << position
+        else:
+            raise ValueError(" Given color must be either black or white!")
 
-        self.board |= 1 << position # The 1(bit) will be moved by the number given below in the main
+        # self.board |= 1 << position # The 1(bit) will be moved by the number given below in the main
     
-    def clear_bit(self, position: int):
+    def clear_bit(self, position: int, color: str):
+        color = color.lower()
         if not (0 <= position <= 63):
             raise ValueError("Given bit is not within the board range!")
 
-        self.board &= ~(1 << position) # After the equal to sign, we give the code in brackets which says move 1 to the position,
+         # After the equal to sign, we give the code in brackets which says move 1 to the position,
                                        # but the NOT symbol inverts it resulting in clearing the bit
                                        # at the equal to sign, we use AND symbol to remove the bit if it is 1 and remain 0 if is was
+        if color == "white":
+            self.white_board &= ~(1 << position)
+        elif color == "black":
+            self.black_board &= ~(1 << position)
+        else:
+            raise ValueError("Given color must be either black or white!")
         ...
     
-    def is_bit_set(self, position: int) -> bool: # As it will return either 0 or 1
+    def is_bit_set(self, position: int, color: str) -> bool: # As it will return either 0 or 1
+        color = color.lower()
         if not (0 <= position <= 63):
             raise ValueError("Given bit is not within the board range!")
 
-        return (self.board & 1 << position) != 0 # It should return 1 if 1 is in the number mentioned of position
+        # It should return 1 if 1 is in the number mentioned of position
+        if color == "white":
+            return (self.white_board & 1 << position) != 0
+        elif color == "black":
+            return (self.black_board & 1 << position) != 0
+        else:
+            raise ValueError("Given color must be either black or white!")
         ...
 
     def print_board(self) -> None:
         """Prints a visual representation of the bitboard (for debugging)."""
-        print("  a b c d e f g h")
+        # color = color.lower()
         for rank in range(7, -1, -1):
             print(f"{rank + 1} ", end='')
             for file in range(8):
                 position = rank * 8 + file
-                print("R" if self.is_bit_set(position) else '0', end=' ') # R for ready, and 0 for not ready, it will be confusing as there will be rank numbering as well
+                if self.is_bit_set(position, 'black'):
+                    print('B', end=' ')
+                elif self.is_bit_set(position, 'white'):
+                    print('W', end=' ')
+                else:
+                    print('0', end=' ')
             print(f"{rank + 1}")
         print("  a b c d e f g h")
 
 def main():
-    bitboard = Bitboard(8)
-    bitboard.set_bit(0)
-    bitboard.set_bit(6)
-    bitboard.set_bit(63)
-    bitboard.set_bit(56)
+    # These are just random positions in which bits are placed, nothing special.
+    bitboard = Bitboard()
+    bitboard.set_bit(0, "white")
+    bitboard.set_bit(63, "black")
+    bitboard.set_bit(53, "black")
+    bitboard.set_bit(10, "white")
+    bitboard.set_bit(43, "black")
+    bitboard.set_bit(20, "white")
     bitboard.print_board()
 
 if __name__ == "__main__":
