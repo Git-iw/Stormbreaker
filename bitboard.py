59a6c9a9005e3c866e971e26746858cfaef4fe0b
Git-iw <141666971+Git-iw@users.noreply.github.com>
2024-06-03 12:50:04 +0100

    Using a dictionary to not use white&black boards. Using this method the pieces are printed correctly

--- bitboard.py
+++ bitboard.py
@@ -1,96 +1,81 @@
 class Bitboard:
-    def __init__(self, black_board=0, white_board=0):
-        self.white_board = white_board 
-        self.black_board = black_board
+    def __init__(self):
+        self.board = {}
         self.pieces = ["pawn", "knight", "bishop", "rook", "queen", "king"]
+        self.colors = ["white", "black"]
 
     def set_bit(self, position: int, color: str, piece: str):
         color = color.lower()
         piece = piece.lower()
         if piece not in self.pieces:
             raise ValueError("Given name must be of a piece in chess!")
-        if color == "white":
-            self.white_board |= 1 << position
-        elif color == "black":
-            self.black_board |= 1 << position
-        else:
-            raise ValueError("Given color must be either black or white!")
+        if not (0 <= position <= 63):
+            raise ValueError("Given bit is not within the board range!")
+        self.board[position] = (color, piece)
 
-    def clear_bit(self, position: int, color: str, piece: str):
-        color = color.lower()
-        piece = piece.lower()
+    def clear_bit(self, position: int):
+        if not (0 <= position <= 63):
+            raise ValueError("Given bit is not within the board range!")
+        if position in self.board:
+            del self.board[position]
         # After the equal to sign, we give the code in brackets which says move 1 to the position,
         # but the NOT symbol inverts it resulting in clearing the bit
         # at the equal to sign, we use AND symbol to remove the bit if it is 1 and remain 0 if it was
-        if piece not in self.pieces:
-            raise ValueError("Given name must be of a piece in chess!")
-        if not (0 <= position <= 63):
-            raise ValueError("Given bit is not within the board range!")
-        if color == "white":
-            self.white_board &= ~(1 << position)
-        elif color == "black":
-            self.black_board &= ~(1 << position)
-        else:
-            raise ValueError("Given color must be either black or white!")
-        
-    def is_bit_set(self, position: int, color: str, piece: str) -> bool: # As it will return either 0 or 1
+
+    def is_bit_set(self, position: int, color: str, piece: str) -> bool:
         color = color.lower()
         piece = piece.lower()
         if piece not in self.pieces:
             raise ValueError("Given name must be of a piece in chess!")
         if not (0 <= position <= 63):
             raise ValueError("Given bit is not within the board range!")
-        # It should return 1 if 1 is in the number mentioned of position
-        if color == "white":
-            return (self.white_board & (1 << position)) != 0
-        elif color == "black":
-            return (self.black_board & (1 << position)) != 0
-        else:
-            raise ValueError("Given color must be either black or white!")
+        return self.board.get(position) == (color, piece)
 
     def print_board(self) -> None:
         """Prints a visual representation of the bitboard (for debugging)."""
+        piece_symbols = {
+            'pawn': 'P',
+            'knight': 'N',
+            'bishop': 'B',
+            'rook': 'R',
+            'queen': 'Q',
+            'king': 'K'
+        }
+        print("  a b c d e f g h")
         for rank in range(7, -1, -1):
             print(f"{rank + 1} ", end='')
             for file in range(8):
                 position = rank * 8 + file
-                if self.is_bit_set(position, 'black', 'pawn'):
-                    print('p', end=' ')
-                elif self.is_bit_set(position, 'black', 'knight'):
-                    print('n', end=' ')
-                elif self.is_bit_set(position, 'black', 'bishop'):
-                    print('b', end=' ')
-                elif self.is_bit_set(position, 'black', 'rook'):
-                    print('r', end=' ')
-                elif self.is_bit_set(position, 'black', 'queen'):
-                    print('q', end=' ')
-                elif self.is_bit_set(position, 'black', 'king'):
-                    print('k', end=' ')
-                elif self.is_bit_set(position, 'white', 'pawn'):
-                    print('P', end=' ')
-                elif self.is_bit_set(position, 'white', 'knight'):
-                    print('N', end=' ')
-                elif self.is_bit_set(position, 'white', 'bishop'):
-                    print('B', end=' ')
-                elif self.is_bit_set(position, 'white', 'rook'):
-                    print('R', end=' ')
-                elif self.is_bit_set(position, 'white', 'queen'):
-                    print('Q', end=' ')
-                elif self.is_bit_set(position, 'white', 'king'):
-                    print('p', end=' ')
+                if position in self.board:
+                    color, piece = self.board[position]
+                    symbol = piece_symbols[piece]
+                    if color == 'black':
+                        symbol = symbol.lower()
+                    print(symbol, end=' ')
                 else:
                     print('0', end=' ')
-            print(f"{rank + 1}")
+            print(f" {rank + 1}")
         print("  a b c d e f g h")
 
 def main():
-    # These are just random positions in which bits are placed, nothing special.
-    # Commented out because piece implementation is still left.
+    # Example to set a rook at position 0
     bitboard = Bitboard()
     bitboard.set_bit(0, "white", "rook")
+    bitboard.set_bit(1, "white", "knight")
+    bitboard.set_bit(2, "white", "bishop")
+    bitboard.set_bit(3, "white", "queen")
+    bitboard.set_bit(4, "white", "king")
+    bitboard.set_bit(5, "white", "bishop")
+    bitboard.set_bit(6, "white", "knight")
+    bitboard.set_bit(7, "white", "rook")
     bitboard.set_bit(63, "black", "rook")
-    bitboard.set_bit(3, "white", "king")
+    bitboard.set_bit(62, "black", "knight")
+    bitboard.set_bit(61, "black", "bishop")
     bitboard.set_bit(60, "black", "king")
+    bitboard.set_bit(59, "black", "queen")
+    bitboard.set_bit(58, "black", "bishop")
+    bitboard.set_bit(57, "black", "knight")
+    bitboard.set_bit(56, "black", "rook")
     bitboard.print_board()
 
 if __name__ == "__main__":
